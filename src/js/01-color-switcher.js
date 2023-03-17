a388a8167f56f14fa0958b99f79edfd76685ad2b
ansachuk <ansachuk@icloud.com>
2023-03-17 08:56:53 +0200

    add random color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+// Change value of isSuccess variable to call resolve or reject
+const isSuccess = true;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve('Success! Value passed to resolve function');
+    } else {
+      reject('Error! Error passed to reject function');
+    }
+  }, 2000);
+});
 
